42222cc805723078b561a45338a3a054bb9438e2
Peter Conn <conn.peter@gmail.com>
2014-01-28 08:49:40 +0000

    Added two basic programs for testing

---
+++ Tests/ReadArray.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	int nums[5];
+	for(int i=0; i<5; i++)
+		printf("%d", nums[i]);
+	return 0;
+}
---
+++ Tests/SetArray.c
@@ -0,0 +1,7 @@
+int main(){
+	int nums[5];
+	for(int i=0; i<5; i++)
+		nums[i] = 1;
+
+	return 0;
+}
