9c295ffae0e1b901c583fcebec22119bcc97c749
Peter Conn <conn.peter@gmail.com>
2014-01-29 12:10:48 +0000

    Added Testcases for arrays of structures

---
+++ Tests/ArrayOfStructs.c
@@ -0,0 +1,17 @@
+struct coord{
+	int x;
+	int y;
+};
+typedef struct coord COORD;
+
+struct vector{
+	COORD a;
+	COORD b;
+};
+typedef struct vector VECTOR;
+
+int main(){
+	COORD points[5];
+	VECTOR lines[3];
+	return 0;
+}
