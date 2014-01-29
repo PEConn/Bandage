109ec4ab3aa5a913ff985e6c7f37c15244c9cca4
Peter Conn <conn.peter@gmail.com>
2014-01-29 12:10:28 +0000

    Array allocations are collected and displayed

--- Basic/Pass.cpp
+++ Basic/Pass.cpp
@@ -1,36 +1,87 @@
+#include <set>
+
 #include "llvm/Pass.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DataLayout.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/InstIterator.h"
+
+#include "../Helpers/Helpers.h"
 
 using namespace llvm;
 
 namespace {
-  struct Bandage : public ModulePass{
-    static char ID;
-    Bandage() : ModulePass(ID) {}
+struct Bandage : public ModulePass{
+  static char ID;
+  Bandage() : ModulePass(ID) {}
 
-    virtual bool runOnModule(Module &M) {
-	  for(auto IFunc = M.begin(), EFunc = M.end(); IFunc != EFunc; ++IFunc){
-	    Function *F = IFunc;
+  virtual bool runOnModule(Module &M) {
+    std::set<Instruction *> ArrayAllocs = CollectArrayAllocs(M);
+    std::set<Instruction *> GetElementPtrs = CollectGetElementPtrs(M);
 
-		for(auto IInst = inst_begin(F), EInst = inst_end(F); IInst != EInst; ++IInst){
-		  //Instruction *I = &*IInst;
-          if(GetElementPtrInst *I = dyn_cast<GetElementPtrInst>(&*IInst)){
-		    errs() << *I << "\n";
-		  }
+    //PrintIrWithHighlight(M, ArrayAllocs, GetElementPtrs);
+    TransformArrayAllocs(ArrayAllocs);
 
-		  if(AllocaInst *I = dyn_cast<AllocaInst>(&*IInst)){
-		  	errs() << *I << "\n";
-		  }
-		}
-	  }
+    return true;
+  }
+private:
+  std::set<Instruction *> CollectArrayAllocs(Module &M);
+  std::set<Instruction *> CollectGetElementPtrs(Module &M);
 
-      return false;
+  void TransformArrayAllocs(std::set<Instruction *> ArrayAllocs);
+};
+}
+
+std::set<Instruction *> Bandage::CollectArrayAllocs(Module &M){
+  std::set<Instruction *> ArrayAllocs;
+
+  for(auto IF = M.begin(), EF = M.end(); IF != EF; ++IF){
+    for(auto II = inst_begin(IF), EI = inst_end(IF); II != EI; ++II){
+      Instruction *I = &*II;
+      if(AllocaInst *I = dyn_cast<AllocaInst>(&*II)){
+        if(!II->getType()->isPointerTy())
+          continue;
+        if(!II->getType()->getPointerElementType()->isArrayTy())
+          continue;
+        ArrayAllocs.insert(I);
+      }
     }
-  };
+  }
+  return ArrayAllocs;
+}
+
+std::set<Instruction *> Bandage::CollectGetElementPtrs(Module &M){
+  std::set<Instruction *> GetElementPtrs;
+
+  for(auto IF = M.begin(), EF = M.end(); IF != EF; ++IF){
+    for(auto II = inst_begin(IF), EI = inst_end(IF); II != EI; ++II){
+      if(GetElementPtrInst *I = dyn_cast<GetElementPtrInst>(&*II)){
+        GetElementPtrs.insert(I);
+      }
+    }
+  }
+  return GetElementPtrs;
+}
+
+void Bandage::TransformArrayAllocs(std::set<Instruction *> ArrayAllocs){
+  for(auto I: ArrayAllocs){
+    auto ArrayAlloc = cast<AllocaInst>(I);
+
+    errs() << "Instruction:\t" << *ArrayAlloc << "\n";
+    errs() << "Array Name:\t\t" << ArrayAlloc->getName() << "\n";
+
+    Type *ArrayType = ArrayAlloc->getAllocatedType();
+    errs() << "No of Elements:\t\t" << ArrayType->getVectorNumElements() << "\n";
+    Type *ElementType = ArrayType->getVectorElementType();
+    errs() << "Element Size(p):\t" << ElementType->getPrimitiveSizeInBits() << "\n";
+    errs() << "Element Size(s):\t" << ElementType->getScalarSizeInBits() << "\n";
+    DataLayout DL;
+    errs() << "Element Size(d):\t" << DL.getTypeSizeInBits(ElementType) << "\n";
+    
+  }
 }
 
 char Bandage::ID = 0;
