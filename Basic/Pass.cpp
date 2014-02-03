14b9698f78a96e236897cfd8e1ac83a596445b15
Peter Conn <conn.peter@gmail.com>
2014-02-03 11:15:52 +0000

    Fat Pointers Approach 1

--- Basic/Pass.cpp
+++ Basic/Pass.cpp
@@ -7,9 +7,11 @@
 #include "llvm/IR/Constants.h"
 #include "llvm/IR/DataLayout.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/InstIterator.h"
 
+#include "FatPointer.h"
 #include "../Helpers/Helpers.h"
 
 using namespace llvm;
@@ -29,8 +31,9 @@ struct Bandage : public ModulePass{
     //PrintIrWithHighlight(M, ArrayAllocs, GetElementPtrs);
     //DisplayArrayInformation(ArrayAllocs);
     //DisplayGepInformation(GetElementPtrs);
-    ModifyGeps(GetElementPtrs);
-    ModifyArrayAllocs(ArrayAllocs);
+    //ModifyGeps(GetElementPtrs);
+    //ModifyArrayAllocs(ArrayAllocs);
+    TurnToFatPointers(ArrayAllocs, GetElementPtrs);
 
     return true;
   }
@@ -46,6 +49,9 @@ private:
 
   void ModifyGeps(std::set<Instruction *> GetElementPtrs);
   void ModifyArrayAllocs(std::set<Instruction *> ArrayAllocs);
+
+  void TurnToFatPointers(std::set<Instruction *> ArrayAllocs,
+      std::set<Instruction *> GetElementPtrs);
 };
 }
 
@@ -111,6 +117,7 @@ void Bandage::ModifyArrayAllocs(std::set<Instruction *> ArrayAllocs){
     auto ArrayAlloc = cast<AllocaInst>(I);
     errs() << "Instruction:\t" << *ArrayAlloc << "\n";
 
+    // Print the array address
     BasicBlock::iterator iter = ArrayAlloc;
     iter++;
     IRBuilder<> B(iter);
@@ -118,6 +125,11 @@ void Bandage::ModifyArrayAllocs(std::set<Instruction *> ArrayAllocs){
     Value *PrintString = B.CreateGlobalStringPtr(StringRef(blue + "Arr: %p\n" + reset), 
         "PrintString");
     B.CreateCall2(Print, PrintString, ArrayAlloc, "DebugPrint"); 
+
+    // Print the array length
+    errs() << "Length:\t" << GetNumElementsInArray(ArrayAlloc) << "\n";
+    errs() << "Elem Size:\t" << GetArrayElementSizeInBits(ArrayAlloc, DL) << "\n";
+
   }
 }
 
@@ -136,5 +148,56 @@ void Bandage::ModifyGeps(std::set<Instruction *> GetElementPtrs){
   }
 }
 
+void Bandage::TurnToFatPointers(std::set<Instruction *> ArrayAllocs,
+    std::set<Instruction *> GetElementPtrs){
+  for(auto I: ArrayAllocs){
+    auto ArrayAlloc = cast<AllocaInst>(I);
+    BasicBlock::iterator iter = ArrayAlloc;
+    iter++;
+    IRBuilder<> B(iter);
+
+    FatPointer *FPF = new FatPointer(&B, ArrayAlloc->getContext());
+
+    Value *FP = FPF->createFatPointer();
+    FPF->setFatPointerValue(FP, ArrayAlloc);
+    FPF->setFatPointerBase(FP, ArrayAlloc);
+
+    Value *ArrayLength = FPF->getConstInteger(GetArrayElementSizeInBits(ArrayAlloc, DL));
+    FPF->setFatPointerLength(FP, ArrayLength);
+
+    // Sanity check
+    Value *PrintString = B.CreateGlobalStringPtr(StringRef(blue + "DBG: %p\n" + reset));
+    B.CreateCall2(Print, PrintString, FPF->getFatPointerValue(FP)); 
+    B.CreateCall2(Print, PrintString, FPF->getFatPointerBase(FP)); 
+    B.CreateCall2(Print, PrintString, FPF->getFatPointerLength(FP)); 
+
+	  //ArrayAlloc->replaceAllUsesWith(FP);
+  }
+
+  for(auto I: GetElementPtrs){
+    auto Gep = cast<GetElementPtrInst>(I);
+    IRBuilder<> B(Gep);
+
+    FatPointer *FPF = new FatPointer(&B, Gep->getContext());
+    Value *FP = Gep->getPointerOperand();
+
+    Value *Pointer= FPF->getFatPointerValue(FP);
+
+    // Get the access indicies
+    std::vector<Value *> IdxList;
+    for(auto Idx = Gep->idx_begin(), EIdx = Gep->idx_end(); Idx != EIdx; ++Idx)
+      IdxList.push_back(*Idx);
+    
+    errs() << *Gep << "\n";
+    errs() << *Gep->getPointerOperandType() << "\n";
+    errs() << *Pointer->getType() << "\n";
+    Value *CastPointer = B.CreatePointerCast(Pointer, Gep->getPointerOperandType());
+    Value* NewGep = B.CreateGEP(CastPointer, IdxList);
+    errs() << *NewGep << "\n";
+
+    BasicBlock::iterator iter(Gep);
+    //ReplaceInstWithInst(Gep->getParent()->getInstList(), iter, NewGep);
+  }
+}
 char Bandage::ID = 0;
 static RegisterPass<Bandage> X("bandage", "Bandage Pass", false, false);
