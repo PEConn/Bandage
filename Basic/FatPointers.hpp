f91ac71590db2f8d7b15c6742891b6c125ed8d95
Peter Conn <conn.peter@gmail.com>
2014-03-22 14:20:44 +0000

    Refactored into multiple files

--- Basic/FatPointers.hpp
+++ Basic/FatPointers.hpp
@@ -5,25 +5,13 @@
 #include <map>
 #include "llvm/IR/Instructions.h"
 
+using namespace llvm;
+
 class FatPointers{
 public:
   Type* GetFatPointerType(Type *PointerType);
 private:
   std::map<Type *, Type *> FatPointerTypes;
 };
-
-Type* FatPointers::GetFatPointerType(Type *PointerType){
-  if(FatPointerTypes.count(PointerType) == 1)
-    return FatPointerTypes[PointerType];
-  
-  std::vector<Type *> FatPointerMembers;	
-  FatPointerMembers.push_back(PointerType);
-  FatPointerMembers.push_back(PointerType);
-  FatPointerMembers.push_back(PointerType);
-  Type *FatPointerType = StructType::create(FatPointerMembers, "struct.FatPointer");
-
-  FatPointerTypes[PointerType] = FatPointerType;
-  return FatPointerType;
-}
 
 #endif
