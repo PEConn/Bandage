7e27e01004dc169517343bc815f89f78a2e79598
Peter Conn <conn.peter@gmail.com>
2014-03-22 16:09:03 +0000

    Added test for function return types

---
+++ Tests/FunctionReturns.c
@@ -0,0 +1,31 @@
+// RUN: ./runOn.sh FunctionReturns | /pool/users/pc424/llvm_build/bin/FileCheck %s
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* CreateInt(){
+  int *x;
+  x = malloc(sizeof(int));
+  *x = 28;
+  printf("%d\n", *x);
+  return x;
+}
+
+int main(){
+  int *x;
+  printf("Clean\n");
+  // CHECK: Clean
+
+  x = CreateInt();
+  // CHECK: 28
+
+  printf("%d\n", *x);
+  // CHECK-NOT: OutOfBounds
+  // CHECK: 28
+
+  printf("End\n");
+  // CHECK: End
+
+  free(x);
+	return 0;
+}
