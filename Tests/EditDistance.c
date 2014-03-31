358eecde6bb7b233d140f17f1fe435bca9260239
Peter Conn <conn.peter@gmail.com>
2014-03-31 15:35:32 +0100

    Fixed up the EditDistance testcase - this is to demonstrate a potential real life bug

--- Tests/EditDistance.c
+++ Tests/EditDistance.c
@@ -1,45 +1,45 @@
 // RUN: ./runOn.sh EditDistance | /pool/users/pc424/llvm_build/bin/FileCheck %s
-// RUN: rm EditDistance.bc EditDistance_ban.bc EditDistance_ban.s EditDistance
-// XFAIL: *
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int ComputeEditDistance(const char *w1, const int s1, const char *w2, const int s2){
-    short *grid = malloc(sizeof(short) * s1 * s2);
-    // The grid is s1 wide and s2 tall
-    // The i'th cell across and j'th cell down is given by:
-    //   grid[j*s1 + i]
+  short *grid = malloc(sizeof(short) * s1 * s2);
+  // The grid is s1 wide and s2 tall
+  // The i'th cell across and j'th cell down is given by:
+  //   grid[j*s1 + i]
 
-    for(int i=0; i<s1; i++) grid[i] = i;
-    for(int j=0; j<s2; j++) grid[j * s1] = j;
+  for(int i=0; i<s1; i++) grid[i] = i;
+  for(int j=0; j<s2; j++) grid[j * s1] = j;
 
-    for(int j=1; j<s2; j++){
-        for(int i=1; i<s1; i++){
-            int left    = grid[    j*s1 + (i-1)];
-            int diag    = grid[(j-1)*s1 + (i-1)];
-            int above   = grid[(j-1)*s1 + i    ];
+  for(int j=1; j<s2; j++){
+    for(int i=1; i<s1; i++){
+      int left    = grid[    j*s1 + (i-1)];
+      int diag    = grid[(j-1)*s1 + (i-1)];
+      int above   = grid[(j-1)*s1 + i    ];
 
-            int min = (left < diag ? left : diag);
-            min = (min < above ? min : above);
+      int min = (left < diag ? left : diag);
+      min = (min < above ? min : above);
 
-            if(w1[i] != w2[j]) min++;
+      if(w1[i] != w2[j]) min++;
 
-            grid[j*s1 + i] = min;
-        }
+      grid[j*s1 + i] = min;
     }
+  }
 
-    int ret = grid[s1 * s2 - 1];
-    free(grid);
-    return ret;
+  // Out of Bounds error here!
+  int ret = grid[s1 * s2];
+  //int ret = grid[s1 * s2 - 1];
+  free(grid);
+  return ret;
 }
 
 int main(){
-    char *w1 = "balloon";   int s1 = 7;
-    char *w2 = "baboon";    int s2 = 6;
+  char *w1 = "balloon";   int s1 = 7;
+  char *w2 = "baboon";    int s2 = 6;
 
-    int dist = ComputeEditDistance(w1, s1, w2, s2);
-	// CHECK: The edit distance between balloon and baboon is 2
-    printf("The edit distance between %s and %s is %d\n", w1, w2, dist);
+  int dist = ComputeEditDistance(w1, s1, w2, s2);
+  // CHECK: OutOfBounds
+  printf("The edit distance between %s and %s is %d\n", w1, w2, dist);
 }
 
