6bb13c934c3bd7c25d224e5fe22c7389256e0be5
Peter Conn <conn.peter@gmail.com>
2014-03-31 08:22:18 +0100

    Added a testcase for a trivial function that just returns its parameter (which is a pointer)

---
+++ Tests/FunctionPassThrough.c
@@ -0,0 +1,29 @@
+// RUN: ./runOn.sh FunctionPassThrough | /pool/users/pc424/llvm_build/bin/FileCheck %s
+#include <stdio.h>
+#include <stdlib.h>
+
+char* Func(char* x){
+  return x;
+}
+
+int main(){
+  printf("Clean\n");
+  // CHECK: Clean
+
+  char *word = "bat";
+  word = Func(word);
+  printf("%c\n", *word);
+  // CHECK-NOT: OutOfBounds
+
+  printf("Dirty\n");
+  // CHECK: Dirty
+
+  word = word + 4;
+  printf("%c\n", *word);
+  // CHECK: OutOfBounds
+
+  printf("Finished\n");
+  // CHECK: Finished
+  return 0;
+
+}
